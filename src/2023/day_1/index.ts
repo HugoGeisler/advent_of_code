9c1bc0f24643b6fbb591bf703b8f3ef31456bc48
Hugo Gslr <hugogess@gmail.com>
2023-12-03 15:37:54 +0100

    resolve day 1 - star 2

--- src/2023/day_1/index.ts
+++ src/2023/day_1/index.ts
@@ -6,22 +6,68 @@ import input from "./input.json";
 const getCalibrationValue = (input: string[]): number => {
     return input.reduce((inputAccumulator, value) => {
         // Get all number contains in the input
-        const numberContainsInInput = value
-            .split("")
-            .reduce((valueAccumulator, currentValue) => {
-                if (Number.isInteger(Number(currentValue)))
-                    return [...valueAccumulator, currentValue];
-                return valueAccumulator;
-            }, []);
+        const inputNumbers = value.split("").filter(Number);
 
         // Concat first and last value to get the calibration value
         const calibrationValue = Number(
-            [numberContainsInInput.at(0), numberContainsInInput.at(-1)].join("")
+            [inputNumbers.at(0), inputNumbers.at(-1)].join("")
         );
 
         // Summarize
         return inputAccumulator + calibrationValue;
     }, 0);
+};
+
+const getCalibrationValueWithWordererNumber = (input: string[]): number => {
+    const worderedNumber: string[] = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    const getDigitsInWord = (word: string): number[] => {
+        const numberPresentInWord: number[] = [];
+        while (word.length > 0) {
+            if (Number.isInteger(Number(word[0]))) {
+                numberPresentInWord.push(Number(word[0]));
+                word = word.substring(1);
+            } else {
+                const result = word.match(new RegExp(worderedNumber.join("|")));
+
+                if (result && result.index === 0) {
+                    const resultIndex = worderedNumber.indexOf(result[0]);
+                    numberPresentInWord.push(resultIndex);
+                    word = word.substring(result[0].length - 1);
+                } else {
+                    word = word.substring(1);
+                }
+            }
+        }
+
+        return numberPresentInWord;
+    };
+
+    // Get all number contains in the input
+    return input.reduce<number>((inputAccumulator, value) => {
+        const inputNumbers = getDigitsInWord(value);
+
+        // Concat first and last value to get the calibration value
+        const calibrationValue = Number(
+            [inputNumbers.at(0), inputNumbers.at(-1)].join("")
+        );
+
+        // Summarize
+        return inputAccumulator + calibrationValue;
+    }, 0);
+
+    return 0;
 };
 
 /**
@@ -31,8 +77,16 @@ const main = () => {
     // Get calibration value
     const calibrationValues = getCalibrationValue(input);
 
-    // Show the result
-    console.log(`Result of day 1 :`, calibrationValues);
+    // Get calibration value with worderer number
+    const calibrationValueWithWordererNumber =
+        getCalibrationValueWithWordererNumber(input);
+
+    // Show the results
+    console.log(`Result of day 1 - Star 1 :`, calibrationValues);
+    console.log(
+        `Result of day 1 - Star 2 :`,
+        calibrationValueWithWordererNumber
+    );
 };
 
 // Execute main function
